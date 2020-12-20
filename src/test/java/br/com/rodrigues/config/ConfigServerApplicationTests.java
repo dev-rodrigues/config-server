a7fe0d1fa73efe8401e68fe73838750a6cbef7f5
carlos henrique <carlos.hrq.rodrigues@gmail.com>
2020-12-19 22:58:31 -0300

    configuracoes

---
+++ src/test/java/br/com/rodrigues/config/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rodrigues.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
